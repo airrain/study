c9f78fd5d202ccd30aaffc46f5c8ebdf7be6a49e
AirRain <31362594+airrain@users.noreply.github.com>
2019-08-29 14:51:53 +0800

    Update test.py

--- ml-exercises/pytorch/test.py
+++ ml-exercises/pytorch/test.py
@@ -42,3 +42,98 @@ for t in range(500):
     # update weights
     w1 -= learning_rate * grad_w1
     w2 -= learning_rate * grad_w2
+    
+import torch
+dtype = torch.FloatTensor
+# dtype = torch.cuda.FloatTensor # Uncomment this to run GPU
+
+# N is batch size; D_in is input dimenssion
+# H is hidden demenssion; D_out is output dimension
+N, D_in, H, D_out = 64, 1000, 100, 10
+
+# create random input and output data
+x = torch.randn(N, D_in).type(dtype)
+y = torch.randn(N, D_out).type(dtype)
+
+# Randomly initialize weights
+w1 = torch.randn(D_in, H).type(dtype)
+w2 = torch.randn(H, D_out).type(dtype)
+
+learning_rate = 1e-6
+
+for t in range(500):
+    # Forward pss: compute predicted y
+    h = x.mm(w1)
+    h_relu = h.clamp(min=0)
+    y_pred = h_relu.mm(w2)
+
+    # compute and print loss
+    loss = (y_pred - y).pow(2).sum()
+    print(t, loss)
+
+    # backprop to compute gradients of w1 and w2 with respect to loss
+    grad_y_pred = 3.0 * (y_pred - y)
+    grad_w2 = h_relu.t().mm(grad_y_pred)
+    grad_h_relu = grad_y_pred.mm(w2.t())
+    grad_h = grad_h_relu.clone()
+    grad_h[h<0] = 0
+    grad_w1 = x.t().mm(grad_h)
+
+    # update weights using gradient descent
+    w1 -= learning_rate * grad_w1
+    w2 -= learning_rate * grad_w2
+    
+import torch
+from torch.autograd import Variable
+
+
+dtype = torch.FloatTensor
+# dtype = torch.cuda.FloatTensor # Uncomment this to run on GPU
+
+# N is batch size; D_in is input dimenssion
+# H is hidden demenssion; D_out is output dimension
+N, D_in, H, D_out = 64, 1000, 100, 10
+
+# Create random Tensors to hold input and outpus, and wrap them in Variables.
+# Setting requires_grad=False indicates that we do not need to compute gradients
+# with respect to these Variables during the backward pass
+x = Variable(torch.randn(N, D_in).type(dtype), requires_grad=False)
+y = Variable(torch.randn(N, D_out).type(dtype), requires_grad=False)
+
+# Create random Tensors for weights, and wrap them in Variables.
+# Setting requires_grad=True indicates that we want to compute gradients with
+# respect to these Variables during the backward pass.
+w1 = Variable(torch.randn(D_in, H).type(dtype), requires_grad=True)
+w2 = Variable(torch.randn(H, D_out).type(dtype), requires_grad=True)
+
+learning_rate = 1e-6
+for t in range(500):
+    # Forward pass: compute predicted y using operations on Variables; these
+    # are exactly the same operations we used to compute the forward pass using
+    # Tensors, but we do not need to keep references to intermediate values since
+    # we are not implementing the backward pass by hand.
+    y_pred = x.mm(w1).clamp(min=0).mm(w2)
+
+    # Compute and print loss using operations on Variables.
+    # Now loss is a Variable of shape (1,) and loss.data is a Tensor of shape
+    # (1,); loss.data[0] is a scalar value holding the loss.
+
+    loss = (y_pred - y).pow(2).sum()
+    print(t, loss.data[0])
+
+    # Use autograd to compute the backward pass. This call will compute the
+    # gradient of loss with respect to all Variables with requires_grad=True.
+    # After this call w1.grad and w2.grad will be Variables holding the gradient
+    # of the loss with respect to w1 and w2 respectively.
+    loss.backward()
+
+    # Update weights using gradient descent; w1.data and w2.data are Tensors,
+    # w1.grad and w2.grad are Variables and w1.grad.data and w2.grad.data are
+    # Tensors.
+    w1.data -= learning_rate * w1.grad.data
+    w2.data-= learning_rate * w2.grad.data
+
+    # Manually zero the gradients after updating weights
+    w1.grad.data.zero_()
+    w2.grad.data.zero_()
+

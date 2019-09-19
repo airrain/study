6d159c1a5fe217c8d9bdc2e33006a682a11c02ad
AirRain <31362594+airrain@users.noreply.github.com>
2019-09-19 20:26:14 +0800

    update

--- ml-exercises/deeplearning/classifaction.py
+++ ml-exercises/deeplearning/classifaction.py
@@ -2,7 +2,6 @@ import numpy as np
 from sklearn import datasets, linear_model
 import matplotlib.pyplot as plt
 
-
 def generate_data():
     np.random.seed(0)
     X, y = datasets.make_moons(200, noise=0.20)
--- ml-exercises/pytorch/exercise.py
+++ ml-exercises/pytorch/exercise.py
@@ -2,4 +2,7 @@
 #print(a(3))
 
 import cv2
-img = cv2.imread('lena.jpg', 0)
+
+img = cv2.imread(r'C:\Users\airrain\Desktop\jian.jpg', 0)
+cv2.imshow('jian', img)
+cv2.waitKey(0)

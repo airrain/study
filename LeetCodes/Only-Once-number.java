d5c4f11b38421740282973e118b5d68928f307a0
AirRain <31362594+airrain@users.noreply.github.com>
2019-10-09 23:12:17 +0800

    update

--- LeetCodes/Only-Once-number.java
+++ LeetCodes/Only-Once-number.java
@@ -1,3 +1,7 @@
+public class Only-Once-number(){
+	public getNumber(){
+nums = [2,4,5,4,2,5,6]
+
 int ans = nums[0];
 if(nums.length > 1){
 	for(int i = 1;i < nums.length;i++){
@@ -5,3 +9,6 @@ if(nums.length > 1){
 	}
 }
 return ans;
+}
+System.out,println(ans)
+}
